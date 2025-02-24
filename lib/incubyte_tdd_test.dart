056aa188b936ffdfd62f8fa3e2326af712e2e20b
GJJ2019 <gauravjajoo98@gmail.com>
2025-02-24 16:02:51 +0530

    feat: string calculator with . separator

--- lib/incubyte_tdd_test.dart
+++ lib/incubyte_tdd_test.dart
@@ -1,3 +1,11 @@
-int calculate() {
-  return 6 * 7;
+int add(String numbers) {
+  if (numbers.isEmpty) {
+    return 0;
+  }
+  var sum = 0;
+  var numbersList = numbers.split(",");
+  for (var number in numbersList) {
+    sum += int.parse(number);
+  }
+  return sum;
 }
--- test/incubyte_tdd_test_test.dart
+++ test/incubyte_tdd_test_test.dart
@@ -2,7 +2,15 @@ import 'package:incubyte_tdd_test/incubyte_tdd_test.dart';
 import 'package:test/test.dart';
 
 void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
+  test('Input: “”, Output: 0', () {
+    expect(add(''), 0);
+  });
+
+  test('Input: “1”, Output: 1', () {
+    expect(add('1'), 1);
+  });
+
+  test('Input: “1,5”, Output: 6', () {
+    expect(add('1,5'), 6);
   });
 }
