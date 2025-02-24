481648318c641c134b320c7606a733f40974405e
GJJ2019 <gauravjajoo98@gmail.com>
2025-02-24 16:12:55 +0530

    feat: code refactor

--- lib/incubyte_tdd_test.dart
+++ lib/incubyte_tdd_test.dart
@@ -3,20 +3,18 @@ int add(String numbers) {
     return 0;
   }
   var sum = 0;
+  List<String> numbersList;
 
   if (numbers.startsWith('//')) {
     var delimiter = numbers[2];
     numbers = numbers.split('\n')[1];
-    var numbersList = numbers.split(delimiter);
-    for (var number in numbersList) {
-      sum += int.parse(number);
-    }
+    numbersList = numbers.split(delimiter);
   } else {
-    var numbersList = numbers.split(RegExp(r',|\n'));
-    for (var number in numbersList) {
-      sum += int.parse(number);
-    }
+    numbersList = numbers.split(RegExp(r',|\n'));
   }
 
+  for (var number in numbersList) {
+    sum += int.parse(number);
+  }
   return sum;
 }
